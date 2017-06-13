5ffbab0506aaaa1a0fcb4510e83ab231ea190861
haoyun299 <657996689@qq.com>
2017-06-13 21:24:58 +0800

    add page

---
+++ js/common.js
@@ -0,0 +1,45 @@
+$(function(){
+    var method = {
+        grid:function(){
+            $(".grid").isotope({
+                itemSelector: '.grid-item',
+                percentPosition: true
+            });
+        },
+        navTab:function(){
+            $('a[data-toggle="tab"]').on('shown.bs.tab',function () {
+                method.grid();
+            });
+        },
+        panelCollapse:function () {
+            $('.collapse').on('shown.bs.collapse', function () {
+                method.grid();
+            });
+        },
+        accordion:function () {
+            $(".voice_2 ul li").each(function(){
+                var fold = $(this).find(".fold");
+                // $('.voice_2 li:first').find(".fold").hide();
+                // $('.voice_2 li:first').find(".unfold").show();
+                if(fold.is(":hidden")){
+                    $(this).width("58%");
+                }else{
+                    $(this).width("7%");
+                }
+            });
+            $(".voice_2 ul li").click(function(){
+                var li_index = $(this).index();
+                $(this).animate({width:"58%"},200);
+                $(this).find(".unfold").show();
+                $(this).find(".fold").hide();
+                $(this).siblings().animate({width:"7%"},200);
+                $(this).siblings().find(".unfold").hide();
+                $(this).siblings().find(".fold").show();
+            });
+        }
+    }
+    method.navTab();
+    method.panelCollapse();
+    method.accordion();
+    method.grid();
+});
